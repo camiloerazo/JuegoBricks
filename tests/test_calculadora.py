fb73338971fe59f4bbc467aed7ff7d2bf7aea984
Camilo Erazo <38138409+camiloerazo@users.noreply.github.com>
2025-03-26 00:43:57 +0000

    4 .py archives created in /src

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,5 +1,8 @@
 import pytest
-from src.calculadora import *  # Importa el módulo desde el paquete src
+from src.calculadora import sumar
 
-def test_placeholder():
-    assert True  # TODO: Reemplazar con pruebas reales
+def test_sumar():
+    assert sumar(2, 3) == 5
+    assert sumar(-1, 1) == 0
+    assert sumar(0, 0) == 0
+    assert sumar(100, 200) == 300
